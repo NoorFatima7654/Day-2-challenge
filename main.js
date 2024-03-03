c98fa7d5044b0abaaccaa1d6b75798b0bf672697
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-03 10:37:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Question no1
+// let author: string = "Albert Einstein"
+// console.log(`"${author}" once said ,"A person who never made a mistake never tried anything new"`,)
+// Question no 2
+// let person_name:string = "Albert Eientein"
+// let message: string =` "${person_name} ,A person  who never made a mistake never tried anything new"`
+// console.log(message);
+//question no 3
+var Name = " \t\n  Arish  \t\n";
+console.log(Name);
+console.log(Name.trim());
